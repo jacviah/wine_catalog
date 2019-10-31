ed72b49f0edfd83658b59f30dd5020ef75a9bd5b
jacviah <jacviah@gmail.com>
2019-10-31 17:11:42 +0200

    no message

--- dao/src/main/java/by/jacviah/winery/dao/entity/BottleEntity.java
+++ dao/src/main/java/by/jacviah/winery/dao/entity/BottleEntity.java
@@ -10,25 +10,90 @@ public class BottleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
-    private Long id;
+    private final Long id;
 
     @ManyToOne
     @JoinColumn(name = "wine_id")
-    private WineEntity wine;
+    private final WineEntity wine;
     //User user;
 
     @Column(name = "year")
-    private Year year;
+    private final Year year;
 
     @Column(name = "status")
-    private boolean isDrunk;
+    private final boolean isDrunk;
 
     @Column(name = "date")
-    private LocalDate date;
+    private final LocalDate date;
 
     @Column(name = "rate")
-    private String rate;
+    private final String rate;
 
-    public BottleEntity() {
+    public BottleEntity(Builder builder) {
+        this.id = builder.id;
+        this.wine = builder.wine;
+        this.year = builder.year;
+        this.isDrunk = builder.isDrunk;
+        this.date = builder.date;
+        this.rate = builder.rate;
+    }
+
+    public static class Builder {
+        private Long id;
+        private WineEntity wine;
+        //User user;
+        private Year year;
+        private boolean isDrunk;
+        private LocalDate date;
+        private String rate;
+
+
+        public static Builder newInstance() {
+            return new Builder();
+        }
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setWine(WineEntity wine) {
+            this.wine = wine;
+            return this;
+        }
+
+        public Builder setYear(Year year) {
+            this.year = year;
+            return this;
+        }
+        public Builder setStatus(boolean isDrunk) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder setRate(String rate) {
+            this.rate = rate;
+            return this;
+        }
+
+        public BottleEntity build() {
+            return new BottleEntity(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BottleEntity{" +
+                "wine=" + wine +
+                ", year=" + year +
+                ", isDrunk=" + isDrunk +
+                ", date=" + date +
+                ", rate='" + rate + '\'' +
+                '}';
     }
 }
---
+++ dao/src/main/java/by/jacviah/winery/dao/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package by.jacviah.winery.dao.entity;
+
+public class UserEntity {
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final String uuid;
+    private final String role;
+
+
+}
