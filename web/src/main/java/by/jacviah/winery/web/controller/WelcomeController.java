b2434e0656bb6e69837dbc141d34f977486a16f7
HageN\jacviah <jacviah@gmail.com>
2019-12-16 02:59:15 +0300

    no message

--- web/src/main/java/by/jacviah/winery/web/controller/WelcomeController.java
+++ web/src/main/java/by/jacviah/winery/web/controller/WelcomeController.java
@@ -3,10 +3,11 @@ package by.jacviah.winery.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-    @GetMapping(value = "/")
+    @GetMapping(value = "/home")
     public String home(Model model) {
         return "home";
     }
--- web/src/main/java/by/jacviah/winery/web/spring/WebConfig.java
+++ web/src/main/java/by/jacviah/winery/web/spring/WebConfig.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private ServiceConfig serviceConfig;
