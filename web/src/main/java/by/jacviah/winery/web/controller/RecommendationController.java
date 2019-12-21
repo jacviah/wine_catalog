5eb1f0e91309aa602ced2d46622dbc0f18989049
HageN\jacviah <jacviah@gmail.com>
2019-12-22 02:12:28 +0300

    no message

--- web/src/main/java/by/jacviah/winery/web/controller/RecommendationController.java
+++ web/src/main/java/by/jacviah/winery/web/controller/RecommendationController.java
@@ -7,9 +7,11 @@ import by.jacviah.winery.sevice.RecommendService;
 import by.jacviah.winery.sevice.UserService;
 import by.jacviah.winery.sevice.WineService;
 import by.jacviah.winery.web.rq.SimpleUserForm;
+import by.jacviah.winery.web.rq.WineForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Controller
 public class RecommendationController {
     private static final Logger log = LoggerFactory.getLogger(SubscribeController.class);
 
@@ -42,21 +45,24 @@ public class RecommendationController {
         return "subscribers";
     }
 
-    @GetMapping("/recommendation/{user}")
+    @GetMapping("/subscribers/{user}")
     public String getSubscribersAndWines(@PathVariable String user, ModelMap map) {
         List<Wine> wines = wineService.getWines();
+        List<WineForm> result = wines.stream().map(wine -> new WineForm(wine)).collect(Collectors.toList());
         map.addAttribute("username", user);
-        map.addAttribute("wines", wines);
+        map.addAttribute("wines", result);
         return "recommendation";
     }
 
     @PostMapping("/recommendation")
     public String createRecommendation(@RequestParam String username,
                                        @RequestParam String description,
-                                       @RequestParam Set<Wine> wines,
+                                       @RequestParam Set<WineForm> wineForms,
                                        UsernamePasswordAuthenticationToken authentication,
                                        ModelMap map)  {
-
+        Set<Wine> wines = wineForms.stream()
+                .map(wineForm -> wineService.findWine(wineForm.getName(), wineForm.getWinery()))
+                .collect(Collectors.toSet());
         Recommendation rec = Recommendation.RecommendationBuilder.aRecommendation()
                 .withMessage(description)
                 .withSommelier(((User) authentication.getPrincipal()))
---
+++ web/src/main/java/by/jacviah/winery/web/rq/WineForm.java
@@ -0,0 +1,49 @@
+package by.jacviah.winery.web.rq;
+
+import by.jacviah.winery.model.Wine;
+
+public class WineForm {
+    String name;
+    String winery;
+    String grape;
+    String region;
+
+    public WineForm(Wine wine) {
+        this.name = wine.getName();
+        this.winery = wine.getWinery();
+        this.grape = wine.getGrape().getName();
+        this.region = wine.getRegion().getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWinery() {
+        return winery;
+    }
+
+    public void setWinery(String winery) {
+        this.winery = winery;
+    }
+
+    public String getGrape() {
+        return grape;
+    }
+
+    public void setGrape(String grape) {
+        this.grape = grape;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
